8113529bbb8f2a7eb085b455cb28a9b3b5ea5571
Plot <49232758+Plot1337@users.noreply.github.com>
2023-10-30 11:58:32 +1000

    Add .NET Framework test app

---
+++ Tests/ConsoleAppFW/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleAppFW
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, world!");
+            Console.ReadKey(true);
+        }
+    }
+}

8915a94100d2b451de18ffeeb3e8e3c77844f112
Plot <49232758+Plot1337@users.noreply.github.com>
2023-10-30 11:58:13 +1000

    Update ObfuscationSettings.cs

--- Tests/ConsoleApp/ObfuscationSettings.cs
+++ Tests/ConsoleApp/ObfuscationSettings.cs
@@ -1,4 +1,3 @@
 ﻿using System.Reflection;
 
-[assembly: Obfuscation(Feature = "code control flow obfuscation", Exclude = false)]
 [assembly: Obfuscation(Feature = "apply to type *: apply to member * when method or constructor: virtualization", Exclude = false)]

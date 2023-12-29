1ae8b940842e2d0a01081ee49f5109bf7ace5422
Plot <49232758+Plot1337@users.noreply.github.com>
2023-12-29 15:00:50 +1000

    Update Program.cs
    Don't use top-level statements, improve code, update to support latest Eaz.NET version, remove useless code, improve patching

--- EazTrialRemover/Program.cs
+++ EazTrialRemover/Program.cs
@@ -2,260 +2,193 @@ using dnlib.DotNet;
 using dnlib.DotNet.Emit;
 using System.Diagnostics;
 
-namespace EazTrialRemover;
+Console.Title = "Eaz.NET Trial Remover - Plot1337";
 
-internal class Program
+if (args.Length == 0)
 {
-    private static void Main(string[] args)
+    Exit("Usage: EazTrialRemover <assembly>");
+    return;
+}
+
+int len = args.Length;
+
+Console.WriteLine(
+    "Loaded {0} {1}!",
+    len, len > 1 ? "assemblies" : "assembly"
+    );
+
+var sw = Stopwatch.StartNew();
+
+foreach (string path in args)
+{
+    sw.Reset();
+    sw.Start();
+
+    WriteColored("Attempting to patch: ", ConsoleColor.Blue);
+    Console.WriteLine(path);
+
+    if (!path.EndsWith("exe") && !path.EndsWith("dll"))
     {
-        Console.Title = "Eaz.NET Trial Remover - Plot1337";
-
-        if (args.Length == 0)
-        {
-            Exit("Usage: EazTrialRemover <assembly>");
-            return;
-        }
-
-        int len = args.Length;
-        Console.WriteLine($"Loaded {len} {(len > 1 ? "assemblies" : "assembly")}!");
-
-        foreach (string path in args)
-        {
-            var sw = Stopwatch.StartNew();
-            sw.Start();
-
-            WriteColored("Attempting to patch: ", ConsoleColor.Blue);
-            Console.WriteLine(path);
-
-            if (!path.EndsWith("exe") && !path.EndsWith("dll"))
-            {
-                Exit("Invalid input!");
-                continue;
-            }
-            else if (!File.Exists(path))
-            {
-                Exit("Assembly not found!");
-                continue;
-            }
-
-            try { Patch(path); }
-            catch { WriteLnColored("Failed to patch!", ConsoleColor.Red); }
-            finally
-            {
-                sw.Stop();
-
-                WriteLnColored(
-                    $"Task completed in {sw.Elapsed.TotalSeconds} seconds!",
-                    ConsoleColor.Yellow
-                    );
-            }
-        }
-
-        Exit();
+        Exit("Invalid input!");
+        continue;
+    }
+    else if (!File.Exists(path))
+    {
+        Exit("Assembly not found!");
+        continue;
     }
 
-    private static void Patch(string path)
+    try { Patch(path); }
+    catch (Exception ex)
     {
-        var module = ModuleDefMD.Load(path);
-        var types = module.GetTypes();
-
-        bool
-            patchedCheck = false,
-            patchedVoid = false;
-
-        for (int i = 0; i < types.Count(); i++)
-        {
-            var type = types.ElementAt(i);
-
-            if (patchedCheck && patchedVoid)
-                break;
-            else if (!type.HasMethods)
-                continue;
-
-            if (!patchedCheck)
-                patchedCheck = PatchCheckMethod(ref type);
-
-            if (!patchedVoid)
-                patchedVoid = PatchVoidMethod(ref type);
-        }
-
-        /* TODO: Look into this */
-        if (patchedCheck || patchedVoid) Save(module, path);
-        else WriteLnColored("Failed to patch!", ConsoleColor.Red);
+        WriteLnColored("Failed to patch!", ConsoleColor.Red);
+        WriteLnColored(ex.ToString(), ConsoleColor.Red);
     }
 
-    private static bool PatchCheckMethod(ref TypeDef type)
-    {
-        bool ok = TryFindMethod(
-            type,
-            "System.Boolean",
-            3,
-            out var body,
-            out var insts
-            );
+    sw.Stop();
 
-        if (!ok || body == null || insts == null)
-            return false;
-        else if (
-            insts[1].OpCode == OpCodes.Call &&
-            insts[2].OpCode == OpCodes.Ret
-            )
+    WriteLnColored(
+        $"Task completed in {sw.Elapsed.TotalSeconds} seconds!",
+        ConsoleColor.Yellow
+        );
+}
+
+Exit();
+
+static void Patch(string path)
+{
+    var module = ModuleDefMD.Load(path);
+    var types = module.GetTypes().ToList();
+
+    for (int i = 0; i < types.Count; i++)
+    {
+        var type = types[i];
+
+        if (!IsTrialCheckClass(type))
+            continue;
+
+        Console.Write($"Found trial-check class: ");
+        WriteLnColored("0x" + type.MDToken, ConsoleColor.Red);
+
+        var methods = type.Methods;
+
+        foreach (var method in methods)
         {
-            Console.Write("Found check method: ");
-            WriteLnColored("0x" + type.MDToken, ConsoleColor.Red);
+            if (!method.HasBody)
+                continue;
+
+            var insts = method.Body.Instructions;
 
             insts.Clear();
-            insts.Add(OpCodes.Ldc_I4_1.ToInstruction());
+            method.Body.ExceptionHandlers.Clear();
+
+            if (MethodRets(method, "Boolean"))
+                insts.Add(OpCodes.Ldc_I4_1.ToInstruction());
+
             insts.Add(OpCodes.Ret.ToInstruction());
-
-            WriteLnColored("Patched check method!", ConsoleColor.Green);
-            return true;
         }
-        else return false;
+
+        Save(module, path);
+        return;
     }
 
-    private static bool PatchVoidMethod(ref TypeDef type)
-    {
-        bool ok = TryFindMethod(
-            type,
-            "System.Void",
-            4,
-            out var body,
-            out var insts
-            );
+    WriteLnColored("Failed to patch!", ConsoleColor.Red);
+}
 
-        if (!ok || body == null || insts == null)
-            return false;
-        else if (
-            insts[1].OpCode == OpCodes.Call &&
-            insts[2].OpCode == OpCodes.Pop
-            )
+static bool IsTrialCheckClass(TypeDef type)
+{
+    if (!type.HasMethods)
+        return false;
+
+    var methods = type.Methods;
+
+    if (methods.Count != 4)
+        return false;
+
+    bool methodsRetsBool = true, methodsRetsVoid = true;
+
+    for (int i = 0; i < 4; i++)
+    {
+        if (i < 2)
         {
-            Console.Write("Found void method: ");
-            WriteLnColored("0x" + type.MDToken, ConsoleColor.Red);
-
-            insts.Clear();
-            insts.Add(OpCodes.Ret.ToInstruction());
-
-            WriteLnColored("Patched void method!", ConsoleColor.Green);
-            return true;
+            if (!MethodRets(methods[i], "Boolean"))
+                methodsRetsBool = false;
         }
-        else return false;
-    }
-
-    private static bool TryFindMethod(
-        TypeDef type,
-        string ret,
-        int instsLen,
-        out CilBody? body,
-        out IList<Instruction>? insts
-        )
-    {
-        body = null;
-        insts = null;
-
-        var methods = FilterMethods(type, ret);
-
-        if (methods.Count != 1)
-            return false;
-
-        var method = methods.First();
-
-        if (method == null || !method.HasBody)
-            return false;
-
-        body = method.Body;
-
-        if (!body.HasInstructions)
-            return false;
-
-        insts = body.Instructions;
-        return insts.Count == instsLen;
-    }
-
-    private static List<MethodDef> FilterMethods(
-        TypeDef type,
-        string ret,
-        params string[] input
-        )
-    {
-        int len = input.Length;
-        var result = new List<MethodDef>();
-
-        foreach (var method in type.Methods)
+        else
         {
-            var metPerms = method.Parameters;
-            int metLen = metPerms.Count;
-
-            if (
-                !method.IsStatic ||
-                method.ReturnType.FullName != ret ||
-                input.Length != metLen
-                ) continue;
-
-            bool match = true;
-
-            for (int i = 0; i < len && i < metLen; i++)
-                if (metPerms[i].Type.FullName != input[i])
-                {
-                    match = false;
-                    break;
-                }
-
-            if (match)
-                result.Add(method);
+            if (!MethodRets(methods[i]))
+                methodsRetsVoid = false;
         }
-
-        return result;
     }
 
-    private static void Save(ModuleDefMD module, string path)
+    return methodsRetsBool && methodsRetsVoid;
+}
+
+static bool MethodRets(
+    MethodDef method,
+    string? retType = null
+    )
+{
+    if (!method.HasReturnType)
+        return string.IsNullOrEmpty(retType) || retType == "Void";
+
+    return ("System." + retType) == method.ReturnType.FullName;
+}
+
+static void Save(ModuleDefMD module, string path)
+{
+    WriteLnColored("Saving...");
+
+    string
+        name = path.EndsWith("exe") ? "exe" : "dll",
+        bakFile = path.Replace('.' + name, "_backup." + name);
+
+    if (File.Exists(bakFile))
+        File.Delete(bakFile);
+
+    File.Move(path, bakFile);
+
+    WriteColored(
+        "Original assembly moved to: ",
+        ConsoleColor.Blue
+        );
+
+    Console.WriteLine(bakFile);
+
+    module.Write(path, new(module)
     {
-        WriteLnColored("Saving...");
+        AddCheckSum = true
+    });
 
-        string
-            name = path.EndsWith("exe") ? "exe" : "dll",
-            bakFile = path.Replace('.' + name, "_backup." + name);
+    WriteColored("Saved to: ", ConsoleColor.Blue);
+    Console.WriteLine(path);
+}
 
-        File.Move(path, bakFile);
+static void WriteLnColored(
+    string text,
+    ConsoleColor color = ConsoleColor.Green
+    ) => WriteColored(text + "\n", color);
 
-        WriteColored("Original assembly moved to: ", ConsoleColor.Blue);
-        Console.WriteLine(bakFile);
+static void WriteColored(
+    string text,
+    ConsoleColor color = ConsoleColor.Green
+    )
+{
+    Console.ForegroundColor = color;
+    Console.Write(text);
+    Console.ResetColor();
+}
 
-        module.Write(path);
-
-        WriteColored("Saved to: ", ConsoleColor.Blue);
-        Console.WriteLine(path);
-    }
-
-    private static void WriteLnColored(
-        string text,
-        ConsoleColor color = ConsoleColor.Green
-        ) => WriteColored(text + "\n", color);
-
-    private static void WriteColored(
-        string text,
-        ConsoleColor color = ConsoleColor.Green
-        )
+static void Exit(string? message = null)
+{
+    if (!string.IsNullOrEmpty(message))
     {
-        Console.ForegroundColor = color;
-        Console.Write(text);
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine(message);
         Console.ResetColor();
     }
 
-    private static void Exit(string? message = null, int code = 0)
-    {
-        if (!string.IsNullOrEmpty(message))
-        {
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine(message);
-            Console.ResetColor();
-        }
-
-        Console.WriteLine();
-        Console.WriteLine("--");
-        Console.WriteLine("Press any key to exit...");
-        Console.ReadKey(true);
-        Environment.Exit(code);
-    }
+    Console.WriteLine("\n--\nPress any key to exit...");
+    Console.ReadKey(true);
+    Environment.Exit(0);
 }
